89e88b0851607fdc9a076b89a56be89193fc58d9
TawhidSajib <tauhidulsajib34@gmail.com>
2021-04-22 19:45:19 +0600

    name and skill type section done by fetch api

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function myFunction() {
     }
 }
 
-fetch('service.json')
+fetch('user.json')
     .then(res => {
         return res.json()
     })
@@ -24,10 +24,22 @@ fetch('service.json')
 
         let workColumn = document.querySelectorAll('.workColumn')
         data.images.forEach((user, index) => {
-            workColumn[index].innerHTML +=
-                `
-                <img class="workImg" src="${user.image}" height="100%" width="100%">
+            workColumn[index].innerHTML += `<img class="workImg" src="${user.image}" height="100%" width="100%">`
+        })
 
-                 `
+        let name = document.querySelector('.name')
+        let phone = document.querySelector('.phone')
+        let email = document.querySelector('.email')
+        data.info.forEach(user => {
+            name.innerHTML += `I am <strong>${user.name}</strong>`
+            phone.innerHTML += `<span>Phone:</span> ${user.phone} `
+            email.innerHTML += `<span>Email:</span> ${user.email} `
+        })
+
+        let skillName = document.querySelectorAll('.skillName')
+        let skill = document.querySelectorAll('.skill')
+        data.skills.forEach((user, index) => {
+            skillName[index].innerHTML += `${user.name}`
+            skill[index].innerHTML += `${user.progress}`
         })
     })
